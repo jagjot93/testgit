dc6efbe2a5f2a59f59a716f64407a66b5a187d3e
jagjot <root@ip-172-31-70-19.ec2.internal>
2020-09-27 13:55:01 +0000

    fresh

---
+++ function.c
@@ -0,0 +1,21 @@
+Main()
+{
+        Function ()
+        {
+                //Initial Code
+        }
+        Function3 ()
+        {
+                //Test code
+        }
+}
+
+	Function ()
+	{
+		//Initial Code
+	}
+	Function2 ()
+	{
+		//Dev code
+	}
+}
